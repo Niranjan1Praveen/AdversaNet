d779d5399d84641be0463197df048662367a1819
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-05-30 12:37:37 +0530

    Updated Files

--- Prototype/client/src/generated/prisma/index.js
+++ Prototype/client/src/generated/prisma/index.js
@@ -127,7 +127,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\nithi\\Desktop\\NTCC-2025\\Prototype\\client\\src\\generated\\prisma",
+      "value": "/Users/serb9/Desktop/NTCC-TermPaper/NTCC-2025/Prototype/client/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -136,17 +136,16 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "darwin",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\nithi\\Desktop\\NTCC-2025\\Prototype\\client\\prisma\\schema.prisma",
+    "sourceFilePath": "/Users/serb9/Desktop/NTCC-TermPaper/NTCC-2025/Prototype/client/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.8.1",
@@ -155,7 +154,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
@@ -204,8 +203,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "src/generated/prisma/query_engine-windows.dll.node")
+path.join(__dirname, "libquery_engine-darwin.dylib.node");
+path.join(process.cwd(), "src/generated/prisma/libquery_engine-darwin.dylib.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "src/generated/prisma/schema.prisma")
